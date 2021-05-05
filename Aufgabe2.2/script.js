23e75d075867e94c098e9820940720aa6d6339c8
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-05 15:25:47 +0200

    Update script.js
    Zu viele verbesserungen um aufzuzählen...

--- Aufgabe2.2/script.js
+++ Aufgabe2.2/script.js
@@ -2,40 +2,36 @@
 var Aufgabe_1;
 (function (Aufgabe_1) {
     /* a) */
-    let numberArray = [17, 10, 23, 2, 66];
-    console.log(min(numberArray));
-    function min(numberArray) {
-        let min = Math.min.apply(null, numberArray);
-        return (min);
+    console.log(min(17, 10, 23, 2, 66));
+    function min(...numberArray) {
+        return Math.min.apply(null, numberArray);
     }
     /* b) */
-    if (isEven(0)) {
-        console.log("Ungerade");
-    }
-    else {
-        console.log("Gerade");
-    }
+    console.log(isEven(50));
+    console.log(isEven(75));
+    //console.log(isEven(-1));
     function isEven(evenTest) {
-        let evenTestBoolean;
-        if (evenTest % 2) {
-            evenTestBoolean = false;
+        if (evenTest == 0) {
+            return true;
         }
-        if (evenTest % 2) {
-            evenTestBoolean = true;
+        if (evenTest == 1) {
+            return false;
         }
-        return (evenTestBoolean);
+        return isEven(evenTest - 2);
     }
     let person1 = { name: "Ludwig", alter: 19, matrikelnummer: 266612 };
     let person2 = { name: "Walter", alter: 22, matrikelnummer: 245687 };
     let person3 = { name: "Billhardt", alter: 21, matrikelnummer: 267213 };
+    let studentArray = [person1, person2, person3, { name: "André", matrikelnummer: 193021, alter: 21 }];
+    console.log(studentArray[3].name, studentArray[0].alter, studentArray[1].matrikelnummer);
     function ausgabePerson(namedPerson) {
         console.log(namedPerson.name);
         console.log(namedPerson.alter);
         console.log(namedPerson.matrikelnummer);
     }
-    ausgabePerson(person1);
-    ausgabePerson(person2);
-    ausgabePerson(person3);
+    for (let info of studentArray) {
+        ausgabePerson(info);
+    }
 })(Aufgabe_1 || (Aufgabe_1 = {}));
 var Aufgabe_1_c_mitKlasse;
 (function (Aufgabe_1_c_mitKlasse) {
@@ -61,22 +57,31 @@ var Aufgabe_2;
     /* a) */
     console.log(backwards([1, 2, 3, 4, 5]));
     function backwards(umdrehArray) {
-        let arrayReversed = umdrehArray;
-        const reversed = arrayReversed.reverse();
-        return (reversed);
+        let backwardsArray = [];
+        for (let i = 0; i < umdrehArray.length; i++) {
+            backwardsArray[i] = umdrehArray[umdrehArray.length - i - 1];
+        }
+        return backwardsArray;
     }
     /* b) */
-    console.log(join(["a", "b", "c"], ["d", "e", "f"]));
-    function join(addArray, addArray2) {
-        const arrayJoined = addArray.concat(addArray2);
-        return (arrayJoined);
+    console.log(join([1, 2, 3], [4, 5, 6]));
+    function join(arr, arr2) {
+        while (arr2.length > 0) {
+            arr.push(arr2.shift());
+        }
+        return arr;
     }
     /* c) */
     console.log(split([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2, 4));
     function split(splitArray, index1, index2) {
-        let ausschnittArray = splitArray;
-        ausschnittArray = ausschnittArray.slice(index1, index2);
-        return (ausschnittArray);
+        let ausschnittArray = [];
+        if (index1 > 0 && index2 < splitArray.length && index1 < index2) {
+            for (let i = index1 - 1; i < index2; i++) {
+                ausschnittArray.push(splitArray[i]);
+            }
+            return ausschnittArray;
+        }
+        return ausschnittArray;
     }
 })(Aufgabe_2 || (Aufgabe_2 = {}));
 var Aufgabe_3;
@@ -97,21 +102,21 @@ var Aufgabe_3;
     context.stroke();
     context.beginPath();
     context.arc(300, 230, 30, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
+    context.fillStyle = "green";
     context.fill();
     context.lineWidth = 5;
-    context.strokeStyle = '#003300';
+    context.strokeStyle = "#003300";
     context.stroke();
     context.beginPath();
     context.fillStyle = "black";
     context.fillRect(100, 200, 100, 100);
     context.stroke();
     context.beginPath();
-    context.strokeStyle = '#ff0000';
+    context.strokeStyle = "#ff0000";
     context.moveTo(100, 200);
     context.lineTo(150, 150);
     context.lineTo(200, 200);
-    context.fillStyle = '#ff0000';
+    context.fillStyle = "#ff0000";
     context.fill();
     context.closePath();
     context.stroke();
@@ -121,10 +126,10 @@ var Aufgabe_3;
     context.arc(100, 70, 30, 0, 2 * Math.PI, false);
     context.arc(110, 50, 30, 0, 2 * Math.PI, false);
     context.arc(120, 60, 30, 0, 2 * Math.PI, false);
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fill();
     context.lineWidth = 5;
-    context.strokeStyle = '#ffffff';
+    context.strokeStyle = "#fffff";
     context.stroke();
     context.beginPath();
     context.arc(300, 40, 30, 0, 2 * Math.PI, false);
@@ -132,11 +137,20 @@ var Aufgabe_3;
     context.arc(360, 50, 30, 0, 2 * Math.PI, false);
     context.arc(320, 40, 30, 0, 2 * Math.PI, false);
     context.arc(330, 60, 30, 0, 2 * Math.PI, false);
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fill();
     context.lineWidth = 5;
-    context.strokeStyle = '#ffffff';
+    context.strokeStyle = "#ffffff";
     context.stroke();
+    //Beispiel rechteck:
+    const recht1 = {
+        width: 200,
+        height: 300,
+        positionX: 1000,
+        positionY: 1000,
+        color: "black",
+        lineColor: "green"
+    };
     /* c) */
     createRect();
     function createRect() {
@@ -144,14 +158,14 @@ var Aufgabe_3;
         let randomHeight = Math.floor(Math.random() * (300 - 50)) + 50;
         let randomX = Math.floor(Math.random() * (1000 - 100)) + 100;
         let randomY = Math.floor(Math.random() * (1000 - 50)) + 400;
-        return [randomX, randomY, randomWidth, randomHeight];
+        let rechteck1 = { width: randomWidth, height: randomHeight, positionX: randomX, positionY: randomY };
+        return rechteck1;
     }
     /* d) */
-    //drawRect(createRect());
     drawRect(createRect());
-    function drawRect(rectArray) {
+    function drawRect(rectCreate) {
         context.fillStyle = "black";
-        context.fillRect(rectArray[0], rectArray[1], rectArray[2], rectArray[3]);
+        context.fillRect(rectCreate.positionX, rectCreate.positionY, rectCreate.width, rectCreate.height);
     }
     /* e) */
     let arrayArray = [createRect(), createRect(), createRect(), createRect(), createRect()];
