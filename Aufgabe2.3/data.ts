f48152b904b5301d137f786a37c0375dd5fc4278
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-09 17:49:21 +0200

    Create data.ts

---
+++ Aufgabe2.3/data.ts
@@ -0,0 +1,7 @@
+namespace Aufgabe_2 {
+
+    export let spitze1: Spitze[] = [{ name: "spitze1", farbe: "rot", preis: 100, länge: 10 }, {name: "spitze2", farbe: "grün", preis: 200, länge: 20 }, {name: "spitze3", farbe: "blau", preis: 300, länge: 30 }] ;
+    export let kapsel1: Kapsel[] = [{ name: "kapsel1", farbe: "blau", preis: 200, kapaziät: 5 }] ;
+    export let antrieb1: Antrieb[] = [{ name: "antrieb1", farbe: "grün", preis: 100, treibstoff: 500 }] ;
+
+}
