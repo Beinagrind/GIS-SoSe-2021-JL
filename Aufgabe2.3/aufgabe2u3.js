74a0b03c5f584e21abf8a4ad270fe6f84a73f6a1
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-14 09:57:04 +0200

    Update aufgabe2u3.js

--- Aufgabe2.3/aufgabe2u3.js
+++ Aufgabe2.3/aufgabe2u3.js
@@ -2,26 +2,26 @@
 var Aufgabe_2;
 (function (Aufgabe_2) {
     //Rakete mit 3 segmenten
-    let rakete1 = { _spitze: Aufgabe_2.spitze1, _kapsel: Aufgabe_2.kapsel1, _antrieb: Aufgabe_2.antrieb1 };
     //Aufgabe3
     //ich verstehe nicht wieso das nicht funktioniert, es hat doch bei der ersten aufgabe auch getan...
     //als ichs ohne for schleife getestet hab hats funktioniert...
     //ich hab mir mal einige andere Abgaben angeschaut, aber soweit ich sehen konnte, hat es auf die selbe art funktoniert
     //div soll erstellt werden innerhalb eines bestehnden divvs mit id, und die daten der jeweiligen Spitze im Array anzeigen.
-    //un dann über ein grid layout je nach menge im aray angepasst werden
+    //und dann über ein grid layout je nach menge im aray angepasst werden
     let layout = document.getElementById("wrapper");
-    function anzeigeSpitze() {
-        for (let i; i < Aufgabe_2.spitze1.length; i++) {
+    function anzeigeSpitze(_spitze) {
+        for (let i = 0; i < _spitze.length; i++) {
             let auswahlDiv = document.createElement("div");
             //auswahlDiv.addEventListener("click", onclick);
-            let name = Aufgabe_2.spitze1[i].name;
-            let länge = Aufgabe_2.spitze1[i].länge;
-            let farbe = Aufgabe_2.spitze1[i].farbe;
-            let preis = Aufgabe_2.spitze1[i].preis;
+            let name = _spitze[i].name;
+            let länge = _spitze[i].länge;
+            let farbe = _spitze[i].farbe;
+            let preis = _spitze[i].preis;
             auswahlDiv.appendChild(document.createTextNode(name + " ist " + länge + " lang " + ", " + farbe + " und " + preis + " Währung teuer"));
+            console.log(layout);
             layout.appendChild(auswahlDiv);
         }
     }
-    anzeigeSpitze();
+    anzeigeSpitze(Aufgabe_2.spitze1);
 })(Aufgabe_2 || (Aufgabe_2 = {}));
 //# sourceMappingURL=aufgabe2u3.js.map
