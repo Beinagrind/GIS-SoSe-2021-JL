e136e294f70d960654667b701988887ec09b9418
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-14 09:57:08 +0200

    Update aufgabe2u3.ts

--- Aufgabe2.3/aufgabe2u3.ts
+++ Aufgabe2.3/aufgabe2u3.ts
@@ -31,14 +31,11 @@ export interface Antrieb {
 
 export interface Rakete {
 
-    _spitze: Spitze[];
-    _kapsel: Kapsel[];
-    _antrieb: Antrieb[];
+    spitze: Spitze[];
+    kapsel: Kapsel[];
+    antrieb: Antrieb[];
 
 }
-
-let rakete1: Rakete = {_spitze: spitze1, _kapsel: kapsel1, _antrieb: antrieb1} ;
-
 
 //Aufgabe3
 
@@ -48,31 +45,31 @@ let rakete1: Rakete = {_spitze: spitze1, _kapsel: kapsel1, _antrieb: antrieb1} ;
 
 
 //div soll erstellt werden innerhalb eines bestehnden divvs mit id, und die daten der jeweiligen Spitze im Array anzeigen.
-//un dann über ein grid layout je nach menge im aray angepasst werden
+//und dann über ein grid layout je nach menge im aray angepasst werden
 
 let layout = <HTMLDivElement>document.getElementById("wrapper");
 
-function anzeigeSpitze(): void {
+function anzeigeSpitze(_spitze: Spitze[]): void {
 
-    for (let i: number; i < spitze1.length; i++) {
+    for (let i: number = 0; i < _spitze.length; i++) {
         
         let auswahlDiv: HTMLDivElement = document.createElement("div");
 
         //auswahlDiv.addEventListener("click", onclick);
 
-        let name: string = spitze1[i].name;
-        let länge: number = spitze1[i].länge;
-        let farbe: string = spitze1[i].farbe;
-        let preis: number = spitze1[i].preis;
+        let name: string = _spitze[i].name;
+        let länge: number = _spitze[i].länge;
+        let farbe: string = _spitze[i].farbe;
+        let preis: number = _spitze[i].preis;
 
         auswahlDiv.appendChild(document.createTextNode(name + " ist " + länge + " lang " + ", " + farbe + " und " + preis + " Währung teuer"));
-
+        console.log(layout);
         layout.appendChild(auswahlDiv);
 
     }
 
 }
 
-anzeigeSpitze();
+anzeigeSpitze(spitze1);
 
 }
