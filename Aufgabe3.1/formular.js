138e7e02b672bd1dc6c8d77f4594eb46583898a6
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-29 19:51:06 +0200

    A

--- Aufgabe3.1/formular.js
+++ Aufgabe3.1/formular.js
@@ -1,11 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var P_3_1Server;
 (function (P_3_1Server) {
-    let formData;
     function getButton(_event) {
         serverTest("https://beinagrinddrekifurtwangen.herokuapp.com/");
     }
     async function serverTest(_url) {
+        let formData;
         formData = new FormData(document.forms[0]);
         let query = new URLSearchParams(formData);
         _url = _url + "?" + query.toString();
@@ -21,23 +22,80 @@ var P_3_1Server;
     }
     async function nameUPWSend() {
         let formData = new FormData(document.forms[0]);
-        //console.log(":" + formData.get ("nameInput")); //Konsolenausgabe
+        console.log(":" + formData.get("name")); //Konsolenausgabe
         for (let entry of formData) {
             console.log(entry);
-            console.log("Name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
-            console.log("Passwort: " + entry[1]); //Konsolenausgabe
+            console.log("name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("name: " + entry[1]); //Konsolenausgabe
         }
         let query = new URLSearchParams(formData);
         let _url = "https://beinagrinddrekifurtwangen.herokuapp.com/"; //Verknüpfung mit der herokuapp
         _url = _url + "?" + query.toString(); //Url in String umwandeln
+        console.log(_url); //Konsolenausgabe
         let response = await fetch(_url); // auf url warten
         let antwort = await response.text(); //Auf die antwort warten
         console.log(antwort);
-        let rueckgabe = document.getElementById("erfolgreich"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        let rueckgabe = document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
         rueckgabe.innerText = antwort;
     }
+    /*async function nameUPWSend(): Promise <void> {
+
+        let formData: FormData = new FormData (document.forms[0]);
+
+        formData.append("api-key", "myApiKey");
+
+        for (let key  of  formData.keys()) {
+
+            console.log(key, formData.get(key));
+
+        }
+
+        let _url: RequestInfo = "https://beinagrinddrekifurtwangen.herokuapp.com/";
+        let request = new Request(_url, {
+            body: formData,
+            method: "POST",
+
+        });
+
+        fetch(request)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("Server Response:");
+                console.log(data);
+            })
+            .catch(console.warn)
+        
+
+        function  convert(formData) {
+
+            let obj = {};
+            for (let key of  formData.keys()) {
+                obj[key] = formData.get
+            }
+
+        }
+
+        //console.log(":" + formData.get ("nameInput")); //Konsolenausgabe
+
+        /*for (let entry of formData) {
+            console.log(entry);
+            console.log("Name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("Passwort: " + entry[1]); //Konsolenausgabe
+        }
+
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        let _url: RequestInfo = "https://beinagrinddrekifurtwangen.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+
+        let response: Response = await fetch (_url); // auf url warten
+        let antwort: string = await response.text(); //Auf die antwort warten
+        console.log (antwort);
+
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("erfolgreich"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort;
+        */
     let button = document.getElementById("login");
-    button.addEventListener("click", nameUPWSend);
+    button.addEventListener("submit", nameUPWSend);
     let submit = document.getElementById("get");
     submit.addEventListener("click", getButton);
 })(P_3_1Server || (P_3_1Server = {}));
--- Aufgabe3.1/formular.ts
+++ Aufgabe3.1/formular.ts
@@ -1,8 +1,6 @@
+import { url } from "inspector";
+
 namespace P_3_1Server {
-
-    let formData: FormData;
-
-    
 
     function getButton(_event: Event): void {
 
@@ -12,6 +10,7 @@ namespace P_3_1Server {
 
     async function serverTest(_url: RequestInfo): Promise <void> {
 
+        let formData: FormData;
         formData = new FormData(document.forms[0]);
 
         let query: URLSearchParams = new URLSearchParams(<any>formData);
@@ -29,12 +28,65 @@ namespace P_3_1Server {
         console.log(output);
     }
 
-    async function nameUPWSend(): Promise <void> {
+    async function nameUPWSend(): Promise<void> { //async Funktion
+        let formData: FormData = new FormData (document.forms[0]);
+        console.log(":" + formData.get ("name")); //Konsolenausgabe
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("name: " + entry[1]); //Konsolenausgabe
+        }
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        let _url: RequestInfo = "https://beinagrinddrekifurtwangen.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+        console.log(_url); //Konsolenausgabe
+        let response: Response = await fetch (_url); // auf url warten
+        let antwort: string = await response.text(); //Auf die antwort warten
+        console.log (antwort); 
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort; 
+    }
+
+    /*async function nameUPWSend(): Promise <void> {
 
         let formData: FormData = new FormData (document.forms[0]);
+
+        formData.append("api-key", "myApiKey");
+
+        for (let key  of  formData.keys()) {
+
+            console.log(key, formData.get(key));
+
+        }
+
+        let _url: RequestInfo = "https://beinagrinddrekifurtwangen.herokuapp.com/";
+        let request = new Request(_url, {
+            body: formData,
+            method: "POST",
+
+        });
+
+        fetch(request)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("Server Response:");
+                console.log(data);
+            })
+            .catch(console.warn)
+        
+
+        function  convert(formData) {
+
+            let obj = {};
+            for (let key of  formData.keys()) {
+                obj[key] = formData.get
+            }
+
+        }
+
         //console.log(":" + formData.get ("nameInput")); //Konsolenausgabe
 
-        for (let entry of formData) {
+        /*for (let entry of formData) {
             console.log(entry);
             console.log("Name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
             console.log("Passwort: " + entry[1]); //Konsolenausgabe
@@ -50,11 +102,10 @@ namespace P_3_1Server {
 
         let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("erfolgreich"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
         rueckgabe.innerText = antwort; 
-
-    }
+        */
 
     let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("login"); 
-    button.addEventListener("click", nameUPWSend);
+    button.addEventListener("submit", nameUPWSend);
 
     let submit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("get");
     submit.addEventListener("click", getButton);
