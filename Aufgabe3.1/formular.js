622bf01ac1576e89beb3e8a85fcb8ec2bc0a7b86
Beinagrind <80220493+Beinagrind@users.noreply.github.com>
2021-05-29 19:23:48 +0200

    Weiterarbeit1

--- Aufgabe3.1/formular.js
+++ Aufgabe3.1/formular.js
@@ -2,8 +2,6 @@
 var P_3_1Server;
 (function (P_3_1Server) {
     let formData;
-    let submit = document.getElementById("get");
-    submit.addEventListener("click", getButton);
     function getButton(_event) {
         serverTest("https://beinagrinddrekifurtwangen.herokuapp.com/");
     }
@@ -21,5 +19,26 @@ var P_3_1Server;
         }
         console.log(output);
     }
+    async function nameUPWSend() {
+        let formData = new FormData(document.forms[0]);
+        //console.log(":" + formData.get ("nameInput")); //Konsolenausgabe
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("Name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("Passwort: " + entry[1]); //Konsolenausgabe
+        }
+        let query = new URLSearchParams(formData);
+        let _url = "https://beinagrinddrekifurtwangen.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+        let response = await fetch(_url); // auf url warten
+        let antwort = await response.text(); //Auf die antwort warten
+        console.log(antwort);
+        let rueckgabe = document.getElementById("erfolgreich"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort;
+    }
+    let button = document.getElementById("login");
+    button.addEventListener("click", nameUPWSend);
+    let submit = document.getElementById("get");
+    submit.addEventListener("click", getButton);
 })(P_3_1Server || (P_3_1Server = {}));
 //# sourceMappingURL=formular.js.map
--- Aufgabe3.1/formular.ts
+++ Aufgabe3.1/formular.ts
@@ -2,8 +2,7 @@ namespace P_3_1Server {
 
     let formData: FormData;
 
-    let submit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("get");
-    submit.addEventListener("click", getButton);
+    
 
     function getButton(_event: Event): void {
 
@@ -29,5 +28,35 @@ namespace P_3_1Server {
         }
         console.log(output);
     }
+
+    async function nameUPWSend(): Promise <void> {
+
+        let formData: FormData = new FormData (document.forms[0]);
+        //console.log(":" + formData.get ("nameInput")); //Konsolenausgabe
+
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("Name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("Passwort: " + entry[1]); //Konsolenausgabe
+        }
+
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        let _url: RequestInfo = "https://beinagrinddrekifurtwangen.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+
+        let response: Response = await fetch (_url); // auf url warten
+        let antwort: string = await response.text(); //Auf die antwort warten
+        console.log (antwort); 
+
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("erfolgreich"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort; 
+
+    }
+
+    let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("login"); 
+    button.addEventListener("click", nameUPWSend);
+
+    let submit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("get");
+    submit.addEventListener("click", getButton);
 
 }
